5f50f617956a5b3d2e9c4d6caa557de2505a2421
swar <chaudhariswar123@gmail.com>
2026-01-26 20:39:53 +0530

    added /me route in app

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,16 +1,19 @@
+# app/main.py
+
 from fastapi import FastAPI
 from app.database import Base, engine
 from app.auth import router as auth_router
+from app.user import router as user_router
 
 Base.metadata.create_all(engine)
 
 app = FastAPI(
     title="InternMatch API",
-    description="Backend API for InternMatch",
     version="1.0.0",
 )
 
 app.include_router(auth_router)
+app.include_router(user_router)
 
 
 @app.get("/", tags=["Health"])
